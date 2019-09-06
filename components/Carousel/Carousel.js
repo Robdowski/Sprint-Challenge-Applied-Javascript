e7c6553cc7a63cbd616ba41e98cc5c0e54981167
Robert <rc3331@gmail.com>
2019-09-06 11:35:45 -0500

    committing stretch goal carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,74 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carousel() {
+  const carousel = document.createElement("div")
+  carousel.classList.add("carousel")
+  const buttonleft = document.createElement("div")
+  buttonleft.classList.add("left-button")
+  const buttonright = document.createElement("div")
+  buttonright.classList.add("right-button")
+  const image1 = document.createElement("img")
+  const image2 = document.createElement("img")
+  const image3 = document.createElement("img")
+  const image4 = document.createElement("img")
+
+  carousel.appendChild(buttonleft)
+  carousel.appendChild(buttonright)
+  carousel.appendChild(image1)
+  carousel.appendChild(image2)
+  carousel.appendChild(image3)
+  carousel.appendChild(image4)
+
+  image1.src = "assets/carousel/mountains.jpeg"
+  image2.src = "assets/carousel/computer.jpeg"
+  image3.src = "assets/carousel/trees.jpeg"
+  image4.src = "assets/carousel/turntable.jpeg"
+
+  image1.classList.add("images")
+  image2.classList.add("images")
+  image3.classList.add("images")
+  image4.classList.add("images")
+
+  buttonleft.addEventListener("click", e=>{
+    controls(-1)
+  })
+  buttonright.addEventListener("click", e=>{
+    controls(1)
+  })
+
+  return carousel
+}
+
+carouselContainer = document.querySelector(".carousel-container")
+carouselContainer.appendChild(carousel())
+
+
+
+
+  
+var slideIndex = 1
+showSlides(slideIndex)
+
+function controls(n){
+  showSlides(slideIndex += n)
+}
+
+function showSlides(n){
+  let i;
+  let slides = [...document.getElementsByClassName("images")]
+  console.log(slides)
+  if (n>slides.length) {
+    slideIndex = 1
+  }
+  if (n<1){
+    slideIndex = slides.length
+  }
+  for (i=0; i<slides.length; i++){
+    slides[i].style.display = "none"
+  }
+  slides[slideIndex-1].style.display = "block"
+}
+
+
